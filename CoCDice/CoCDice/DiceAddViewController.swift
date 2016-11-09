4e5f1f11005381a966d10884c1a10e1f5795c936
KazuyaMurase <muratyu0306@gmail.com>
2016-11-09 10:43:03 +0900

    Add files via upload
    ダイスロール用のUI追加

---
+++ CoCDice/CoCDice/DiceAddViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DiceAddViewController.swift
+//  CoCDice
+//
+//  Created by student on 2016/11/09.
+//  Copyright © 2016年 student. All rights reserved.
+//
+
+import UIKit
+
+class DiceAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CoCDice/DiceListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DiceListTableViewCell.swift
+//  CoCDice
+//
+//  Created by student on 2016/11/09.
+//  Copyright © 2016年 student. All rights reserved.
+//
+
+import UIKit
+
+class DiceListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
